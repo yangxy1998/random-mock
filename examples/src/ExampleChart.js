27b38e4e06f4d30c8ba94a96e84f6a47cbd54fe1
杨晓宇 <1329218916@qq.com>
2022-02-20 02:01:21 +0800

    feat: 🎸 choose attributes to watch

--- examples/src/ExampleChart.js
+++ examples/src/ExampleChart.js
@@ -55,8 +55,8 @@ import { AttributeType } from '../../build/random-mock'
 
 export class ExampleChart {
     constructor() {
-        const context = document.getElementById('chart').getContext('2d')
-        this.chart = new Chart(context, {
+        this.context = document.getElementById('chart').getContext('2d')
+        this.chart = new Chart(this.context, {
             type: 'scatter',
             data: {
                 datasets: [
@@ -80,41 +80,61 @@ export class ExampleChart {
                 ]
             },
             options: {
-                scales: {
-                    xAxes: [
-                        {
-                            type: 'linear',
-                            position: 'bottom'
-                        }
-                    ]
-                }
+                scales: {}
             }
         })
     }
     data(attributes, data) {
-        const datasets = []
-        for (let left = 0; left < attributes.length - 1; left++) {
-            for (let right = left + 1; right < attributes.length; right++) {
-                if (
-                    attributes[left].type === AttributeType.Continuous &&
-                    attributes[right].type === AttributeType.Continuous
-                ) {
-                    const color = '#' + ((Math.random() * 0xffffff) << 0).toString(16)
-                    datasets.push({
-                        borderColor: color,
-                        backgroundColor: color,
-                        label: `${attributes[left].name}-${attributes[right].name}`,
-                        data: data.map((value) => {
-                            return {
-                                x: value[attributes[left].name],
-                                y: value[attributes[right].name]
-                            }
-                        })
+        this.dataset = data
+        const menu = document.getElementById('menu')
+        const coutinuous = attributes.filter(
+            (attribute) => attribute.type === AttributeType.Continuous
+        )
+        this.discretes = attributes.filter((attribute) => attribute.type === AttributeType.Discrete)
+        this.principle = this.discretes[0]
+        this.discretes.forEach((discrete, index) => {
+            const button = document.createElement('button')
+            const label = `${discrete.name}|${index}`
+            button.id = label
+            button.innerHTML = label
+            button.onclick = () => {
+                const index = button.id.split('|')[1]
+                this.principle = this.discretes[index]
+            }
+            menu.appendChild(button)
+        })
+        for (let left = 0; left < coutinuous.length - 1; left++) {
+            for (let right = left + 1; right < coutinuous.length; right++) {
+                const button = document.createElement('button')
+                const label = `${coutinuous[left].name}-${coutinuous[right].name}`
+                button.id = label
+                button.innerHTML = label
+                button.onclick = () => {
+                    const attributes = button.id.split('-')
+                    const xName = attributes[0]
+                    const yName = attributes[1]
+                    const datasets = this.principle.distribution.range.map((value) => {
+                        const color = '#' + ((Math.random() * 0xffffff) << 0).toString(16)
+                        return {
+                            borderColor: color,
+                            backgroundColor: color,
+                            label: `${value}`,
+                            data: this.dataset
+                                .filter((item) => item[this.principle.name] === value)
+                                .map((value) => {
+                                    return {
+                                        x: value[xName],
+                                        y: value[yName]
+                                    }
+                                })
+                        }
                     })
+                    console.log(datasets)
+                    this.chart.data.datasets = datasets
+                    this.chart.update()
                 }
+                menu.appendChild(button)
             }
         }
-        this.chart.data.datasets = datasets
-        this.chart.update()
     }
 }
